fff0279e00225c9f3eefd5a9440441dfa101ebbb
NayaSamuel <iamuzomaanieze@gmail.com>
2023-02-16 13:59:35 -0800

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- printing a line using the printf fuction
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+
+}
