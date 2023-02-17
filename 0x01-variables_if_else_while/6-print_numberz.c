8716dbc3958cf7bd138b65846efc5599c5dcb5e8
NayaSamuel <iamuzomaanieze@gmail.com>
2023-02-17 14:33:57 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing of numbers of base 10 starting from 0
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
