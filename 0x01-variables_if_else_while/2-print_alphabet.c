b27fd4edebe49b04e2ec046bbd085408c43ff04e
NayaSamuel <iamuzomaanieze@gmail.com>
2023-02-17 13:13:07 -0800

    alphbet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main fuction
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i - 0; i < 26; i++)
+	{
+		putchar(alp[1]);
+	}
+	putchar("\n");
+	return (0);
+}
