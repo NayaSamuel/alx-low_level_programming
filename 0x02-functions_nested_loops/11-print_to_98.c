6c069496915fc7738bbf1b41b439446e3843ce61
NayaSamuel <iamuzomaanieze@gmail.com>
2023-02-22 16:01:59 -0800

    98 battery streer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,30 +11,36 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0  && n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n < 0)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
